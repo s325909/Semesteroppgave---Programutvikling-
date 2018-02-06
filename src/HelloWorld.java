f751f69cb14630ea3f0344adc5fdc0453f64edbc
Jaso Cyril <s325909@stud.hioa.no>
2018-02-06 10:44:20 +0100

    Test

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main( String[] args ){
+        int a = 7;
+        int b = 5;
+        int c = 6;
+
+        boolean result = (a <= b && b == c) || c >= b;
+
+        System.out.println(result);
+    }
+}
