7ceb9668d37a83435f9289137eceaabbdb932588
Jaso Cyril <s325909@stud.hioa.no>
2018-04-09 10:31:55 +0200

    WADS - Movement

--- ShooterGame/src/entities/Player.java
+++ ShooterGame/src/entities/Player.java
@@ -13,26 +13,30 @@ public class Player extends Movable {
 
     public void movePlayer(KeyEvent keyEvent){
         setMoving();
-        if (keyEvent.getCode() == KeyCode.LEFT) {
+        if (keyEvent.getCode() == KeyCode.LEFT || keyEvent.getCode() == KeyCode.A) {
             goLeft();
-        } else if (keyEvent.getCode() == KeyCode.RIGHT) {
+        } else if (keyEvent.getCode() == KeyCode.RIGHT || keyEvent.getCode() == KeyCode.D) {
             goRight();
-        } else if (keyEvent.getCode() == KeyCode.UP && keyEvent.getCode() != KeyCode.DOWN) {
+        } else if (keyEvent.getCode() == KeyCode.UP && keyEvent.getCode() != KeyCode.DOWN
+                || keyEvent.getCode() == KeyCode.W && keyEvent.getCode() != KeyCode.S) {
             goUp();
-        } else if (keyEvent.getCode() == KeyCode.DOWN && keyEvent.getCode() != KeyCode.UP) {
+        } else if (keyEvent.getCode() == KeyCode.DOWN && keyEvent.getCode() != KeyCode.UP
+                || keyEvent.getCode() == KeyCode.S && keyEvent.getCode() != KeyCode.W) {
             goDown();
         }
 
-        if (keyEvent.getCode() == KeyCode.A) {
+        if (keyEvent.getCode() == KeyCode.Q) {
             setAttack();
         }
     }
 
     public void releasedPlayer(KeyEvent keyEvent){
         setIdle();
-        if (keyEvent.getCode() == KeyCode.LEFT || keyEvent.getCode() == KeyCode.RIGHT) {
+        if (keyEvent.getCode() == KeyCode.LEFT || keyEvent.getCode() == KeyCode.RIGHT
+                || keyEvent.getCode() == KeyCode.A || keyEvent.getCode() == KeyCode.D) {
             stopX();
-        } else if (keyEvent.getCode() == KeyCode.UP || keyEvent.getCode() == KeyCode.DOWN) {
+        } else if (keyEvent.getCode() == KeyCode.UP || keyEvent.getCode() == KeyCode.DOWN
+                || keyEvent.getCode() == KeyCode.W || keyEvent.getCode() == KeyCode.S) {
             stopY();
         }
     }
