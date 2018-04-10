5596731b616f4781258916b71c9016f0ac5812a3
sonbit <32852496+sonbit@users.noreply.github.com>
2018-04-10 13:28:52 +0200

    litt fære zombier

--- ShooterGame/src/gameCode/InitializeGame.java
+++ ShooterGame/src/gameCode/InitializeGame.java
@@ -55,7 +55,7 @@ public class InitializeGame implements Initializable{
         }
 
         try {
-            for (int i = 0; i < 175; i++) {
+            for (int i = 0; i < 10; i++) {
                 enemyList.add(new Zombie("/resources/Zombie/skeleton-idle_", ".png", 17, (int) (Math.random() * 1280), (int) (Math.random() * 720), 100));
                 enemyList.get(i).setSpriteIdle("/resources/Zombie/skeleton-idle_", ".png", 17);
                 enemyList.get(i).setSpriteMoving("/resources/Zombie/skeleton-move_", ".png", 17);
